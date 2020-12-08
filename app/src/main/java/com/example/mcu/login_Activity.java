3b81e93140c0dc8fd8f742c7eab94b6173b5c401
KareemSalah98 <kareemsalah325@gmail.com>
2020-12-08 19:57:40 +0200

    sign up

--- app/src/main/java/com/example/mcu/login_Activity.java
+++ app/src/main/java/com/example/mcu/login_Activity.java
@@ -2,7 +2,9 @@ package com.example.mcu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class login_Activity extends AppCompatActivity {
@@ -14,7 +16,23 @@ public class login_Activity extends AppCompatActivity {
 
         // for full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN );
-
         setContentView(R.layout.activity_login_);
+
+        View callsign_up_Activity = findViewById(R.id.sign_up);
+
+
+        callsign_up_Activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_Activity.this,sign_up_Activity.class);
+                startActivity(intent);
+                
+            }
+        }); {
+           
+
+            }
+        } 
+
+
     }
-}
