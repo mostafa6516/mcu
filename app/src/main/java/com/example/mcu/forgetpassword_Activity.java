cf748c78311e49216e6f9f6a9661a3c6cc5a27f5
KareemSalah98 <kareemsalah325@gmail.com>
2020-12-10 17:56:59 +0200

    forgetpassword_Activity

---
+++ app/src/main/java/com/example/mcu/forgetpassword_Activity.java
@@ -0,0 +1,14 @@
+package com.example.mcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgetpassword_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword_);
+    }
+}
