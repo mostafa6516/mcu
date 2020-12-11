edf3f9e1d64148e13f5787ab42237376467f547f
mostafa6516 <mostafazam7@gmail.com>
2020-12-11 23:59:45 +0200

    1

--- app/src/main/java/com/example/mcu/sign_up_Activity.java
+++ app/src/main/java/com/example/mcu/sign_up_Activity.java
@@ -3,12 +3,40 @@ package com.example.mcu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class sign_up_Activity extends AppCompatActivity {
+
+
+    TextInputLayout username,password,confirm_password,phone_number,e_mail;
+    Button sign_up_btn,back_to_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_);
+
+        // hooks
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirm_password = findViewById(R.id.confirm_password);
+        phone_number = findViewById(R.id.phone_number);
+        e_mail = findViewById(R.id.e_mail);
+        sign_up_btn = findViewById(R.id.sign_up_btn);
+        back_to_login = findViewById(R.id.back_to_login);
+
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
