72c23e2345445cb3295dba51c5c4cf44e033e96f
KareemSalah98 <kareemsalah325@gmail.com>
2020-12-30 22:36:13 +0200

    Update

--- app/src/main/java/com/example/mcu/LocationOwner/retailer_dashboard_Activity.java
+++ app/src/main/java/com/example/mcu/LocationOwner/retailer_dashboard_Activity.java
@@ -1,8 +1,10 @@
 package com.example.mcu.LocationOwner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.mcu.R;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
@@ -10,17 +12,22 @@ import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 public class retailer_dashboard_Activity extends AppCompatActivity {
 
     ChipNavigationBar chipNavigationBar;
+    private Object ChipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_dashboard_);
 
 
+        ChipNavigationBar = findViewById(R.id.bottom_nav_mnue);
+        bottomMenu();
+    }
 
+    private void bottomMenu() {
 
-
+        //ChipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.setOnItemSelectedListener()
 
     }
 }
