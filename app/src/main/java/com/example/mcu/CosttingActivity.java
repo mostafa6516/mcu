d67d1909432ba699473d310a62f7ecfeb05653dd
palala <mariam.salah019@gmail.com>
2021-01-10 15:07:01 -0800

    mariam do cost screen

---
+++ app/src/main/java/com/example/mcu/CosttingActivity.java
@@ -0,0 +1,14 @@
+package com.example.mcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CosttingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costting);
+    }
+}
